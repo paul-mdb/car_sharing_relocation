fa820dabe54ebeb9c88c23d04221c165e55b7e2e
paul-mdb <paul.meddeb@mines-paristech.fr>
2022-07-11 13:44:08 +0200

    fix bug

--- analyse_blocks.py
+++ analyse_blocks.py
@@ -354,7 +354,7 @@ for zipcode in range(75001, 75021):
             ## Tables
 
             def estimated_demand_mean(df, district, start_date, end_date, day):
-                dataframe = time_block(dataframe, start_date, end_date, day)
+                dataframe = time_block(df, start_date, end_date, day)
                 grouped_df = dataframe.groupby(by=['month', 'day_number', 'year']).agg({'year': 'first', 'month': 'first', 'day_number': 'first', 'kibana_duration': ['mean', 'count', 'std']})
                 count = np.array(grouped_df['kibana_duration']['count'].values)
                 durations_means = np.array(grouped_df['kibana_duration']['mean'].values)
